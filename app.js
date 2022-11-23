8ba09b4bbb0e7226ee2cf65b605bed62a71041f8
christian-bayata <franksagie1@gmail.com>
2022-11-23 14:14:22 +0100

    Setup express server and mongoose connection

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const path = require("path");
+const Database = require("./config/database");
+
+/* Initialize express application */
+const app = express();
+app.use(express.json());
+
+/* Connect to the database */
+const connectionString = require("./config/connection");
+new Database(connectionString).connect();
+
+module.exports = app;
